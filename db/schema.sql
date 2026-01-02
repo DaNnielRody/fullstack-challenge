72f736d8a85a31fcd275e875fcbf1ff465666f80
Dan <danielrody@gmail.com>
2026-01-02 09:48:33 -0300

    fix: adicionar unique key no e-mail do usuário

--- db/schema.sql
+++ db/schema.sql
@@ -10,7 +10,7 @@ CREATE TABLE posts (
 CREATE TABLE users (
     id INT PRIMARY KEY AUTO_INCREMENT,
     full_name VARCHAR(100),
-    user_email VARCHAR(100) NOT NULL,
+    user_email VARCHAR(100) NOT NULL UNIQUE KEY,
     user_password VARCHAR(200) NOT NULL
 );
 
