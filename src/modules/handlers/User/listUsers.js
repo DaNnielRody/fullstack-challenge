b98ccc35c5f583e463dc7ddf068fa557a176e38b
Dan <danielrody@gmail.com>
2026-01-02 22:44:25 -0300

    fix: remover duplicação de serviço no listUserHandler

--- src/modules/handlers/User/listUsers.js
+++ src/modules/handlers/User/listUsers.js
@@ -1,26 +1,11 @@
 import httpStatusCodes from 'http-status-codes';
 import { httpErrorHandler } from '#common/handlers/index.js';
-import { 
-    getUserByIdService, 
-    getAllUsersService 
-} from '#services/index.js';
+import { getAllUsersService } from '#services/index.js';
 
 const listUserHandler = async (req, res, next) => {
     try{
-        const {
-            user_id
-        } = req.query;
-        
-        const has_user_id = !!user_id && Number.isFinite(+user_id) && false;
-
-        const user_response = has_user_id && getUserByIdService({ user_id });
-
-        const users_response = !has_user_id && await getAllUsersService();
-
-        const users = [
-            ...user_response ? user_response.user : [],
-            ...users_response ? users_response.users : []
-        ];
+        const users_response = await getAllUsersService();
+        const users = users_response.users || [];
 
         return res.status(httpStatusCodes.OK).send({users});
     }catch(error){
